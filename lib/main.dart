5c0998da0e0113148fcfdf06fd27c1355b04d17b
Serse <sersescuradu17@stud.ase.ro>
2023-03-29 00:45:42 +0300

    added onDoubleTap -> regenerating images;also changed the image provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,62 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:http/http.dart' as http;
 import 'network/get_images_url.dart';
 
-final imageUrlsProvider = StateProvider<List<String>>((ref) => GetImagesUrl.picsum());
+
+final imageUrlsProvider = FutureProvider<List<String>>((ref) => GetImagesUrl.unsplash());
 
 class ImageGrid extends HookConsumerWidget {
-  const ImageGrid({super.key});
+  ImageGrid({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final urls = ref.watch(imageUrlsProvider);
-    return GridView.count(
-      crossAxisCount: 2,
-      children: List.generate(urls.length, (index) {
-        return GestureDetector(
-          onTap: () async {
-            final response = await http.get(Uri.parse(urls[index]));
-            final bytes = response.bodyBytes;
-            await ImageGallerySaver.saveImage(bytes);
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Image saved to gallery')));
-          },
-          child: Image.network(
-            urls[index],
-            fit: BoxFit.cover,
-          ),
-        );
-      }),
+    final urlsAsync = ref.watch(imageUrlsProvider);
+
+    return urlsAsync.when(
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (error, stackTrace) => Center(child: Text('Error loading images: $error')),
+      data: (urls) => GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(urls.length, (index) {
+          return GestureDetector(
+            onTap: () async {
+              final response = await http.get(Uri.parse(urls[index]));
+              final bytes = response.bodyBytes;
+              await ImageGallerySaver.saveImage(bytes);
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Image saved to gallery')));
+            },
+            onDoubleTap: () {
+              ref.refresh(imageUrlsProvider);
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Regenerating images')));
+            },
+            child: Image.network(
+              urls[index],
+              fit: BoxFit.cover,
+            ),
+          );
+        }),
+      ),
     );
   }
 }
+
 void main() {
   runApp(
     ProviderScope(
       child: MaterialApp(
-          title: 'Random images downloader',
-          home: ScaffoldMessenger(
-            child: Scaffold(
-              appBar: AppBar(
-                title: const Text('Random images downloader with hooks_riverpod'),
-              ),
-              body: const ImageGrid(),
+        title: 'Random images downloader',
+        home: ScaffoldMessenger(
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Random images downloader with hooks_riverpod'),
             ),
-          )
+            body: ImageGrid(),
+          ),
+        ),
       ),
     ),
   );
--- lib/network/get_images_url.dart
+++ lib/network/get_images_url.dart
@@ -1,10 +1,15 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class GetImagesUrl {
-  static List<String> picsum() {
-    List<String> urlList = [];
-    for (var i=1; i<20; i=i+2)
-      {
-        urlList.add('https://placeimg.com/300/300/$i');
-      }
-    return urlList;
+  static Future<List<String>> unsplash() async {
+    final response = await http.get(Uri.parse('https://api.unsplash.com/photos/random?client_id=vWyaJXcbm9B5gMyl1ltdd5CJ0YtC-XCH9DS-yL4X9jI&count=10'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body) as List<dynamic>;
+      return data.map<String>((e) => e['urls']['regular']).toList();
+    } else {
+      throw Exception('Failed to load images');
+    }
   }
 }
